5dc5c22be2e9175548a6d9e28fb16847ac870cad
Tpt <thomas@pellissier-tanon.fr>
2017-12-05 17:46:14 +0100

    ImageObject have now for URI the file URI

--- src/main/java/org/wikidata/simplewd/api/CommonsAPI.java
+++ src/main/java/org/wikidata/simplewd/api/CommonsAPI.java
@@ -70,38 +70,39 @@ public class CommonsAPI {
                 for (JsonNode pageNode : queryNode.get("pages")) {
                     if (pageNode.isObject() && pageNode.has("imageinfo")) {
                         for (JsonNode infoNode : pageNode.get("imageinfo")) {
-                            EntityValue image = new EntityValue("http://commons.wikimedia.org/wiki/Special:FilePath/" + title.replace(' ', '_'));
-                            image.addType("ImageObject");
                             try {
-                                image.addClaim(new Claim("contentUrl", new URI(infoNode.get("url").asText())));
-                            } catch (URISyntaxException e) {
-                                LOGGER.warn("Invalid file URI: " + infoNode.get("url").asText());
-                            }
-                            image.addClaim(new Claim("fileFormat", infoNode.get("mime").asText()));
-                            image.addClaim(new Claim("width", infoNode.get("width").asInt()));
-                            image.addClaim(new Claim("height", infoNode.get("height").asInt()));
+                                URI imageURI = new URI(infoNode.get("url").asText());
+                                EntityValue image = new EntityValue(imageURI.toString());
+                                image.addType("ImageObject");
+                                image.addClaim(new Claim("contentUrl", imageURI));
+                                image.addClaim(new Claim("fileFormat", infoNode.get("mime").asText()));
+                                image.addClaim(new Claim("width", infoNode.get("width").asInt()));
+                                image.addClaim(new Claim("height", infoNode.get("height").asInt()));
 
-                            if (infoNode.has("extmetadata")) {
-                                JsonNode metadata = infoNode.get("extmetadata");
-                                String name = "";
-                                if (metadata.has("UsageTerms")) {
-                                    name = metadata.get("UsageTerms").get("value").asText();
-                                }
-                                if (metadata.has("LicenseUrl")) {
-                                    //TODO: map to Wikidata
-                                    try {
-                                        URI licenceURI = new URI(metadata.get("LicenseUrl").get("value").asText());
-                                        EntityValue license = new EntityValue(licenceURI.toString());
-                                        image.addType("CreativeWork");
-                                        license.addClaim(new Claim("name", name));
-                                        license.addClaim(new Claim("url", licenceURI));
-                                        image.addClaim("license", license);
-                                    } catch (URISyntaxException e) {
-                                        LOGGER.warn("Invalid file URI: " + metadata.get("LicenseUrl").get("value").asText());
+                                if (infoNode.has("extmetadata")) {
+                                    JsonNode metadata = infoNode.get("extmetadata");
+                                    String name = "";
+                                    if (metadata.has("UsageTerms")) {
+                                        name = metadata.get("UsageTerms").get("value").asText();
+                                    }
+                                    if (metadata.has("LicenseUrl")) {
+                                        //TODO: map to Wikidata
+                                        try {
+                                            URI licenceURI = new URI(metadata.get("LicenseUrl").get("value").asText());
+                                            EntityValue license = new EntityValue(licenceURI.toString());
+                                            image.addType("CreativeWork");
+                                            license.addClaim(new Claim("name", name));
+                                            license.addClaim(new Claim("url", licenceURI));
+                                            image.addClaim("license", license);
+                                        } catch (URISyntaxException e) {
+                                            LOGGER.warn("Invalid file URI: " + metadata.get("LicenseUrl").get("value").asText());
+                                        }
                                     }
                                 }
+                                return image;
+                            } catch (URISyntaxException e) {
+                                throw new IOException("Invalid file URI: " + infoNode.get("url").asText(), e);
                             }
-                            return image;
                         }
                     }
                 }
